943a95be10029c1b2a7475b3c3c458cc5f4f0cca
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-11 22:13:13 +0530

    initial commit

---
+++ Codeforces/2A.py
@@ -0,0 +1,18 @@
+n=int(input())
+d={}
+m=0
+a=[]
+for i in range(n):
+    x,y=input().split()
+    a.append([x,y])
+    y=int(y)
+    d[x]=d.get(x,0)+y
+m=max(d.values())
+f={}
+for i in range(n):
+   x=a[i][0]
+   y=int(a[i][1])
+   f[x]=f.get(x,0)+y
+   if d[x]==m and f[x]>=m:
+       print(x)
+       exit(0) 
