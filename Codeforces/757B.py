afe8a072c65be268521b34e6c72190fcb65f6b96
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-14 14:14:31 +0530

    initial commit

---
+++ Codeforces/757B.py
@@ -0,0 +1,14 @@
+n=int(input())
+d={}
+l=list(map(int,input().split()))
+for i in range(n):
+    x=int(l[i]**0.5)+1
+    for j in range(2,x):
+        if l[i]%j==0:
+            d[j]=d.get(j,0)+1
+        while l[i]%j==0:
+            l[i]//=j
+    if l[i]>1:
+        d[l[i]]=d.get(l[i],0)+1
+# print(d)
+print(max(1,max(d.values())))
