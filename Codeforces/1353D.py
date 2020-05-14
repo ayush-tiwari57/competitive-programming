4215af2aa6c6feb6b23c428ca9c6a65d552a4b6e
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-15 00:14:34 +0530

    initial commit

---
+++ Codeforces/1353D.py
@@ -0,0 +1,16 @@
+import heapq
+for _ in range(int(input())):
+    n=int(input())
+    a=[0]*(n)
+    h=[(-n,(0,n-1))]
+    i=1
+    while i<=n:
+        len, (l,r)=heapq.heappop(h)
+        mid=(l+r)//2
+        a[mid]=i
+        heapq.heappush(h,(-(mid-l),(l,mid-1)))
+        heapq.heappush(h,(-(r-mid),(mid+1,r)))
+        i+=1
+    for i in range(n):
+        print(a[i],end=' ')
+    print()
