7ef349ada593aed79c076c1cbe8a56f853f22f9f
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-15 14:30:34 +0530

    initial commit

--- Codeforces/1353D.py
+++ Codeforces/1353D.py
@@ -1,16 +1,24 @@
+
+
+import sys
+
+input = sys.stdin.buffer.readline
+
+
 import heapq
-for _ in range(int(input())):
-    n=int(input())
-    a=[0]*(n)
-    h=[(-n,(0,n-1))]
-    i=1
-    while i<=n:
-        len, (l,r)=heapq.heappop(h)
-        mid=(l+r)//2
-        a[mid]=i
-        heapq.heappush(h,(-(mid-l),(l,mid-1)))
-        heapq.heappush(h,(-(r-mid),(mid+1,r)))
-        i+=1
-    for i in range(n):
-        print(a[i],end=' ')
-    print()
+def solution():
+    for _ in range(int(input())):
+        n=int(input())
+        a=[0]*(n)
+        h=[(-n,(0,n-1))]
+        i=1
+        while i<=n:
+            len, (l,r)=heapq.heappop(h)
+            mid=(l+r)//2
+            a[mid]=i
+            heapq.heappush(h,(-(mid-l),(l,mid-1)))
+            heapq.heappush(h,(-(r-mid),(mid+1,r)))
+            i+=1
+        print(*a)
+
+solution()
