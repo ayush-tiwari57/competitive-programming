82778733c88d969026af5ef26640f2b5ffca15dc
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-21 20:03:04 +0530

    initial commit

--- Codeforces/276C.py
+++ Codeforces/276C.py
@@ -11,35 +11,18 @@ def solution():
     n,q=map(int,input().split())
     l=list(map(int,input().split()))
     ql=[0]*(n+1)
-    qa=[]
     for i in range(q):
         x,y=map(int,input().split())
-        qa.append((x,y))
         ql[x-1]+=1
         ql[y]-=1
     for i in range(1,len(ql)):
         ql[i]+=ql[i-1]
-    a=[]
-    for i in range(len(ql)):
-        a.append((ql[i],-i))
-    ans=[0]*n
-    a.sort(reverse=True)
+    
+    ql.sort(reverse=True)
     l.sort(reverse=True)
-    for i in range(n):
-        ind=abs(a[i][1])
-        val=l[i]
-        ans[ind]=val
-    ans.reverse()
-    ans.append(0)
-    ans.reverse()
-    for i in range(1,n+1):
-        ans[i]+=ans[i-1]
-    # print(ans)
     s=0
-    for i in range(q):
-        l=qa[i][0]-1
-        r=qa[i][1]
-        s+=(ans[r]-ans[l])
+    for i in range(n):
+        s+=l[i]*ql[i]
     print(s)
         
 solution()
