17fba015469caf6fc22e9b91b39addad575a5d1f
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-22 22:33:52 +0530

    initial commit

--- Codeforces/799B.py
+++ Codeforces/799B.py
@@ -8,9 +8,28 @@ import sys
 input = sys.stdin.buffer.readline
 
 def solution():
-    for _ in range(int(input())):
-        
-        # This is the main code
+    n=int(input())
+    p=list(map(int,input().split()))
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    f=[0]*n
+    m=int(input())
+    c=list(map(int,input().split()))
+    l=[[],[],[],[]]
+    for i in range(n):
+        l[a[i]]+=[[p[i],i]]
+        if b[i]!=a[i]:
+            l[b[i]]+=[[p[i],i]]
+    for i in range(1,4):
+        l[i]=sorted(l[i])
+    i=[0,0,0,0]
+    ans=[]
+    for x in c:
+        while i[x]<len(l[x]) and f[l[x][i[x]][1]]:
+            i[x]+=1
+        if i[x]>=len(l[x]): ans+=[-1]
+        else: ans+=[l[x][i[x]][0]]; f[l[x][i[x]][1]]=1
+    print(*ans)
 
 
 solution()
