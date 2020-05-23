85de274d005ab10cfe4ae464c61bc7ec2b9e1ac1
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-23 17:04:05 +0530

    initial commit

---
+++ Codeforces/235A.cpp
@@ -0,0 +1,58 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 1000004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+
+void solution(){
+
+    // This is the main code
+    ll n;
+    cin>>n;
+    if(n==1 or n==2) cout<<n;
+    else if (n%2==0 and n%3==0) cout<<(n-1)*(n-2)*(n-3);
+    else if (n%2==0) cout<<n*(n-1)*(n-3);
+    else cout<<n*(n-1)*(n-2);
+
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("E:/CP/input.txt","r",stdin);
+    freopen("E:/CP/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    //cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
