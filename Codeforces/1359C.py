3101997117a9293397712103020486ac33e24a61
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-05-30 23:18:16 +0530

    initial commit

--- Codeforces/1359C.py
+++ Codeforces/1359C.py
@@ -11,6 +11,16 @@ def solution():
     for _ in range(int(input())):
         
         # This is the main code
-
-
+        h,c,t=map(int,input().split())
+        bb=(h+c)/2
+        if t<=bb:
+            print(2)
+        else:
+            k= (h-t)/(2*t-h-c)
+            k=int(k)
+            if abs((k*(h+c)+h)-t*(2*k+1))*(2*k+3)<=abs(((k+1)*(h+c)+h)-t*(2*k+3))*(2*k+1):
+                print(2*k+1)
+            else:
+                print(2*(k+1)+1)
+    
 solution()
