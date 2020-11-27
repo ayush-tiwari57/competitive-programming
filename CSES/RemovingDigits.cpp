3301c0f6dbf102f5a7e1db949804f160eaaf8836
servermonk <ayushktiwari572000@gmail.com>
2020-11-27 21:19:25 +0530

    initial commit

---
+++ CSES/RemovingDigits.cpp
@@ -0,0 +1,70 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair 
+#define all(v) v.begin(), v.end()
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 200004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+
+void solution(){
+
+    // This is the main code
+    int n;
+    cin>>n;
+    vector<int> dp(n+1,INT_MAX);
+    dp[0]=0;
+    forn(i,1,n+1){
+        int x=i;
+        while(x>0){
+            if(x%10==0){
+                x/=10;
+                continue;
+            }
+            dp[i]=min(dp[i-(x%10)]+1,dp[i]);
+            x/=10;
+        }
+        // cout<<i<<" "<<dp[i]<<endl;
+    }
+    cout<<dp[n];
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("/home/servermonk/Public/competitive-programming/input.txt","r",stdin);
+    freopen("/home/servermonk/Public/competitive-programming/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
