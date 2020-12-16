c28d8a0684f7faea618a28557366c495bb7720ee
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-12-16 22:49:00 +0530

    initial commit

--- Codeforces/1462C.py
+++ Codeforces/1462C.py
@@ -10,12 +10,23 @@ Codechef: ayush572000
 def solution():
     
     # This is the main code
-    l=[0,1, 2, 3, 4, 5, 6, 7, 8, 9, 19, 29, 39, 49, 59, 69, 79, 89, 189, 289, 389, 489, 589, 689, 789, 1789, 2789, 3789, 4789, 5789, 6789, 16789, 26789, 36789, 46789, 56789, 156789, 256789, 356789, 456789, 1456789, 2456789, 3456789, 13456789, 23456789, 123456789]
     n=int(input())
-    if(n>45):
+    if n>45:
         print(-1)
         return
-    print(l[n])
+    ans=[]
+    cnt=9
+    while n>cnt:
+        n-=cnt
+        ans.append(cnt)
+        cnt-=1
+    ans.append(n)
+    ans.reverse()
+    l=''
+    for i in ans:
+        l+=str(i)
+    print(l)
+    
 
 t=int(input())
 for _ in range(t):
