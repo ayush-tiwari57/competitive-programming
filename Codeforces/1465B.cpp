8e666ab11cc3531e94015e4a6eed594d9d5520b8
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-12-21 13:24:07 +0530

    initial commit

---
+++ Codeforces/1465B.cpp
@@ -0,0 +1,73 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair 
+#define all(v) v.begin(), v.end()
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 200004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+
+void solution(){
+
+    // This is the main code
+    ll n;
+    cin>>n;
+    int flag=0;
+    while(true){
+        ll x=n;
+        flag=0;
+        while(x>0){
+            int y=x%10;
+            if(y>0){
+                if(n%y!=0){
+                    flag=1;
+                    break;
+                }
+            }
+            x/=10;
+        } 
+        if(flag==0) break;
+        n+=1; 
+    }
+    cout<<n<<endl;
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("E:/CP/input.txt","r",stdin);
+    freopen("E:/CP/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
