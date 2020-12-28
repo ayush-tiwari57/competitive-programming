ab71e36725e24fdabf7303533487352305b49148
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-12-28 23:15:23 +0530

    initial commit

---
+++ Codeforces/1005D.cpp
@@ -0,0 +1,70 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair 
+#define all(v) v.begin(), v.end()
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 200004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+
+void solution(){
+
+    // This is the main code
+    string n;
+    cin>>n;
+    int cnt=0,ans=0;
+    for(auto i: n){
+        if((i-'1'+1)%3==0){
+            ans++;
+            cnt=0;
+        }
+        else{
+            cnt+=i-'1'+1;
+            if(cnt%3==0){
+                ans++;
+                cnt=0;
+            }
+        }
+    }
+    cout<<ans;
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("E:/CP/input.txt","r",stdin);
+    freopen("E:/CP/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
---
+++ Codeforces/1469D.cpp
@@ -0,0 +1,68 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair 
+#define all(v) v.begin(), v.end()
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 200004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+
+void solution(){
+
+    // This is the main code
+    int n;
+    cin>>n;
+    vector<pair<int,int>> ans;
+    while(n>2){
+        forn(i,ceil(sqrt(n))+1,n){
+            ans.pb({i,n});
+        }
+        ans.pb({n,ceil(sqrt(n))});
+        ans.pb({n,ceil(sqrt(n))});
+        n=ceil(sqrt(n));
+    }
+    cout<<ans.size()<<endl;
+    forn(i,0,(int)ans.size()){
+        cout<<ans[i].first<<" "<<ans[i].second<<endl;
+    }
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("E:/CP/input.txt","r",stdin);
+    freopen("E:/CP/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
