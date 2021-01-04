54b401652ab8f03cc82bdfc75b05f9f6273445f5
ayushtiwari57 <ayushtiwari572000@gmail.com>
2021-01-04 17:42:09 +0530

    initial commit

---
+++ Codeforces/1472A.py
@@ -0,0 +1,93 @@
+"""
+Code of Ayush Tiwari
+Codeforces: servermonk
+Codechef: ayush572000
+
+"""
+#import sys
+#input = sys.stdin.buffer.readline
+
+#Fast IO
+import os, sys
+from io import IOBase, BytesIO
+py2 = round(0.5)
+if py2:
+    from future_builtins import ascii, filter, hex, map, oct, zip
+    range = xrange
+BUFSIZE = 8192
+class FastIO(BytesIO):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.writable = "x" in file.mode or "w" in file.mode
+        self.write = super(FastIO, self).write if self.writable else None
+ 
+    def _fill(self):
+        s = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+        self.seek((self.tell(), self.seek(0,2), super(FastIO, self).write(s))[0])
+        return s
+ 
+    def read(self):
+        while self._fill(): pass
+        return super(FastIO,self).read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            s = self._fill(); self.newlines = s.count(b"\n") + (not s)
+        self.newlines -= 1
+        return super(FastIO, self).readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.getvalue())
+            self.truncate(0), self.seek(0)
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        if py2:
+            self.write = self.buffer.write
+            self.read = self.buffer.read
+            self.readline = self.buffer.readline
+        else:
+            self.write = lambda s:self.buffer.write(s.encode('ascii'))
+            self.read = lambda:self.buffer.read().decode('ascii')
+            self.readline = lambda:self.buffer.readline().decode('ascii')
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+ 
+# Cout implemented in Python
+import sys
+class ostream:
+    def __lshift__(self,a):
+        sys.stdout.write(str(a))
+        return self
+cout = ostream()
+endl = '\n'
+
+
+
+def solution():
+    
+    # This is the main code
+    h,w,n=map(int,input().split())
+    ans=1
+    while h%2==0:
+        h//=2
+        ans*=2
+    while w%2==0:
+        w//=2
+        ans*=2
+    if ans>=n:
+        print('YES')
+    else:
+        print('NO')
+t=int(input())
+for _ in range(t):
+    solution()
---
+++ Codeforces/1472E.cpp
@@ -0,0 +1,65 @@
+/* code of Ayush Tiwari
+    codeforces- servermonk
+    codechef- ayush572000
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+//STL
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair 
+#define all(v) v.begin(), v.end()
+
+//loops
+#define forn(i,a,b) for(int i=a;  i<b; i++)
+#define rforn(i,a,b) for(int i=a; i>=b; i--)
+
+// defined values
+#define maxn 200004
+#define Mod 1000000007
+
+// fast io
+#define FIO() ios_base::sync_with_stdio(0);cin.tie(0);
+
+
+using namespace std;
+
+bool comp(pair<ll,ll> p1,pair<ll,ll> p2){
+    if(p1.first>p2.first) return true;
+    return false;
+}
+
+void solution(){
+
+    // This is the main code
+    int n,u,v;
+    vector<pair<ll,ll>> h,w;
+    forn(i,0,n){
+        cin>>u>>v;
+        h.pb({u,i});
+        w.pb({v,i});
+    }  
+    
+}
+
+
+int main(){
+    
+    #ifndef ONLINE_JUDGE
+    freopen("E:/CP/input.txt","r",stdin);
+    freopen("E:/CP/output.txt","w",stdout);
+    #endif  
+    
+    FIO()   
+    
+    ll t=1;
+    cin>>t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
