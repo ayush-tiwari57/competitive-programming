bab9ef6f330492c3d56d7665d0bef8df48459ce9
ayushtiwari57 <ayushtiwari572000@gmail.com>
2021-01-27 18:00:59 +0530

    initial commit

--- generate.c
+++ generate.c
@@ -10,11 +10,11 @@ int main() {
     FILE *file;
     file = fopen("./input.txt","w");
     srand(time(0));
-    int n=1111;
+    int n=1;
     int arr[n+1];
-    fprintf(file, "%d\n", 1111);
+    fprintf(file, "%d\n", n);
     for (int i=n; i>=1; i--) 
-        fprintf(file, "%d ", rand()%(10000)+1);
+        fprintf(file, "%d ", rand()%(1000)+1);
     
     return 0;
 }
