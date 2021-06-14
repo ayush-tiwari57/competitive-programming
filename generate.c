9fa529142f82e5841424d046bfb16846268eb950
servermonk <ayushtiwari572000@gmail.com>
2021-06-14 15:54:54 +0530

    initial commit

--- generate.c
+++ generate.c
@@ -10,11 +10,11 @@ int main() {
     FILE *file;
     file = fopen("./input.txt","w");
     srand(time(0));
-    int n=1;
+    int n=10000;
     int arr[n+1];
     fprintf(file, "%d\n", n);
     for (int i=n; i>=1; i--) 
-        fprintf(file, "%d ", rand()%(1000)+1);
+        fprintf(file, "%d ", 1);
     
     return 0;
 }
